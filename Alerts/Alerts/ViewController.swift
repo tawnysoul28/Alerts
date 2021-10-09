01e81acf1ddfa822195da20de774943e085ce4a1
Асанцев Владимир Дмитриевич <v.asantsev@cft.ru>
2021-10-09 20:13:10 +0700

    added alert

--- Alerts/Alerts/ViewController.swift
+++ Alerts/Alerts/ViewController.swift
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Photo Access", message: "Do you allow to use your photos?", preferredStyle: .alert)
+        
+        
+        let okAction = UIAlertAction(title: "Allow", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showActionSheet() {
+    }
+    
 }
 
